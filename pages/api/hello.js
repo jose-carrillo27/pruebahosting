c2ad7167713c3064332fa0b03bf3a36222ed2821
jose.carrillo27 <jose.carrillo27@hotmail.com>
2023-07-05 09:05:33 -0500

    agregando rutina edge

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { cookies } from "next/headers";
+
+export const runtime = "edge";
+
+export async function GET(request) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
+
+  return new Response("Hello, Next.js!", {
+    status: 200,
+    headers: { "Set-Cookie": `token=${token}` },
+  });
 }
